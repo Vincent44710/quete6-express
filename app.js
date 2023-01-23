b54fc816bfe931f6b7d271056266ac0e49aa1841
Vincent <vinto0410@gmail.com>
2023-01-23 10:59:31 +0100

    delete

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
-
+require("dotenv").config();
 const app = express();
+app.use(express.json());
 
-const port = 5000;
+
+
+const port = process.env.APP_PORT ?? 5000;
 
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
@@ -15,9 +18,19 @@ const userHandlers = require("./userHandlers");
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
+app.post("/api/movies",movieHandlers.postMovie);
+app.put("/api/movies/:id", movieHandlers.updateMovie);
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
+
 
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
+app.post("/api/users",userHandlers.postUser);
+app.put("/api/users/:id", userHandlers.updateUser);
+app.delete("/api/users/:id", userHandlers.deleteUser);
+
+movieHandlers
+userHandlers
 
 app.listen(port, (err) => {
   if (err) {
@@ -26,3 +39,7 @@ app.listen(port, (err) => {
     console.log(`Server is listening on ${port}`);
   }
 });
+
+
+
+
--- userHandlers.js
+++ userHandlers.js
@@ -32,7 +32,78 @@ const getUserById = (req, res) => {
     });
 };
 
+
+const postUser = (req, res) => {
+  const { firstname, lastname, email, city, language } = req.body;
+  
+  database
+    .query(
+      "INSERT INTO users(firstname, lastname, email, city, language) VALUES (?, ?, ?, ?, ?)",
+      [firstname, lastname, email, city, language]
+    )
+    .then(([result]) => {
+      res.location(`/api/users/${result.insertId}`).sendStatus(201);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(404).send("No found");
+    });
+};
+
+
+
+
+
+const updateUser = (req, res) => {
+  const id = parseInt(req.params.id);
+  const { firstname, lastname, email, city, language } = req.body;
+
+  database
+    .query(
+      "update movies set firstname = ?, lastname = ?, email = ?, city = ?, language = ? where id = ?",
+      [id, firstname, lastname, email, city, language]
+    )
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send("Not Found");
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error editing the user");
+    });
+};
+
+
+
+const deleteUser = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+    .query("delete from users where id = ?", [id])
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send("Not Found");
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error deleting the user");
+    });
+};
+
+
+
+
+
 module.exports = {
   getUsers,
   getUserById,
+  postUser,
+  updateUser,
+  deleteUser
 };
