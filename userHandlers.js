e4ea306d93c1a19e52a81d138798ceafd22c512e
Vincent <vinto0410@gmail.com>
2023-01-23 15:41:07 +0100

    ok

--- userHandlers.js
+++ userHandlers.js
@@ -1,30 +1,52 @@
 const database = require("./database");
 
-
 const getUsers = (req, res) => {
+  const initialSql = "select * from users";
+  const where = [];
+
+  if (req.query.language != null) {
+    where.push({
+      column: "language",
+      value: req.query.language,
+      operator: "=",
+    });
+  }
+  if (req.query.city != null) {
+    where.push({
+      column: "city",
+      value: req.query.city,
+      operator: "=",
+    });
+  }
+
   database
-    .query("select * from users")
+    .query(
+      where.reduce(
+        (sql, { column, operator }, index) =>
+          `${sql} ${index === 0 ? "where" : "and"} ${column} ${operator} ?`,
+        initialSql
+      ),
+      where.map(({ value }) => value)
+    )
     .then(([users]) => {
-      res.status(200).json(users);
+      res.json(users);
     })
     .catch((err) => {
       console.error(err);
       res.status(500).send("Error retrieving data from database");
     });
 };
-
 
 const getUserById = (req, res) => {
   const id = parseInt(req.params.id);
   database
     .query("select * from users where id = ?", [id])
     .then(([users]) => {
-      if (users.length === 0){
-        res.status(404).send("Not found")
-      }else{
-        res.status(200).json(users[0])
-      };
-      
+      if (users.length === 0) {
+        res.status(404).send("Not found");
+      } else {
+        res.status(200).json(users[0]);
+      }
     })
     .catch((err) => {
       console.error(err);
@@ -32,10 +54,9 @@ const getUserById = (req, res) => {
     });
 };
 
-
 const postUser = (req, res) => {
   const { firstname, lastname, email, city, language } = req.body;
-  
+
   database
     .query(
       "INSERT INTO users(firstname, lastname, email, city, language) VALUES (?, ?, ?, ?, ?)",
@@ -49,10 +70,6 @@ const postUser = (req, res) => {
       res.status(404).send("No found");
     });
 };
-
-
-
-
 
 const updateUser = (req, res) => {
   const id = parseInt(req.params.id);
@@ -76,8 +93,6 @@ const updateUser = (req, res) => {
     });
 };
 
-
-
 const deleteUser = (req, res) => {
   const id = parseInt(req.params.id);
 
@@ -96,14 +111,10 @@ const deleteUser = (req, res) => {
     });
 };
 
-
-
-
-
 module.exports = {
   getUsers,
   getUserById,
   postUser,
   updateUser,
-  deleteUser
+  deleteUser,
 };
