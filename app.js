b15aa1700a0b0a898eba8adffc07887fbb9da3c6
Vincent <vinto0410@gmail.com>
2023-02-07 10:00:20 +0100

    quete 8

--- app.js
+++ app.js
@@ -1,34 +1,54 @@
-const express = require("express");
-const { hashPassword } = require("./auth.js");
 require("dotenv").config();
+
+
+const express = require("express");
+
+const { hashPassword, verifyPassword, verifyToken } = require("./auth");
+
 const app = express();
-app.use(express.json());
-
-
 
 const port = process.env.APP_PORT ?? 5000;
 
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
 };
+app.use(express.json());
 
 app.get("/", welcome);
 
 const movieHandlers = require("./movieHandlers");
+
 const userHandlers = require("./userHandlers");
+
+
+const isItDwight = (req, res) => {
+  if (req.body.email === "dwight@theoffice.com" && req.body.password === "123456") {
+    res.send("Credentials are valid");
+  } else {
+    res.sendStatus(401);
+  }
+};
+
+
+
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
-app.post("/api/movies",movieHandlers.postMovie);
+app.use(verifyToken);
+app.post("/api/movies", verifyToken, movieHandlers.postMovie);
 app.put("/api/movies/:id", movieHandlers.updateMovie);
 app.delete("/api/movies/:id", movieHandlers.deleteMovie);
 
 
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
-app.post("/api/users", hashPassword,userHandlers.postUser);
-app.put("/api/users/:id", hashPassword, userHandlers.updateUser);
-app.delete("/api/users/:id", userHandlers.deleteUser);
+app.post("/api/users", verifyToken,hashPassword,userHandlers.postUser);
+app.put("/api/users/:id", verifyToken, hashPassword, userHandlers.updateUser);
+app.delete("/api/users/:id", verifyToken, userHandlers.deleteUser);
+app.use(verifyToken);
+
+app.post("/api/login",userHandlers.getUserByEmailWithPasswordAndPassToNext,verifyPassword);
+
 
 movieHandlers
 userHandlers
